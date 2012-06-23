00d2ca57c8bbc9d74603ff62a66b685ae56d5701
Ron Gross <ron.gross@gmail.com>
2012-06-23 13:55:30 +0300

    Enable memcached in production

--- app/models/BTCWeightedRates.java
+++ app/models/BTCWeightedRates.java
@@ -1,16 +1,19 @@
 package models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Map;
 
 import static com.google.common.collect.Maps.newHashMap;
 
-public class BTCWeightedRates {
+public class BTCWeightedRates implements Serializable {
     private Map<WeightPeriod, BigDecimal> weights = newHashMap();
+
     public BigDecimal get(WeightPeriod period) {
         return weights.get(period);
     }
-    public void set (WeightPeriod period, BigDecimal rate) {
+
+    public void set(WeightPeriod period, BigDecimal rate) {
         weights.put(period, rate);
     }
 }
